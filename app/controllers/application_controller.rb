efbc01324111b122b2f1cce55faf92ecd5bb4b9c
BennyHallett <benny.hallett@gmail.com>
2015-04-10 08:09:43 +1000

    Username on top bar after login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,6 @@ class ApplicationController < ActionController::Base
   def current_user=(user)
     @current_user = user
   end
+
+  helper_method :current_user
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,7 +2,8 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   Dotenv::Railtie.load
 
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'], {
-    secure_image_url: true
+    secure_image_url: true,
+    image_size: 'mini'
   }
   provider :facebook, "FACEBOOK_KEY", "FACEBOOK_SECRET", {
   }
